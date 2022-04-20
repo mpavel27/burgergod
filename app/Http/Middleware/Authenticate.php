641c92f4b7efdf31cf01c57b2a111dce0c057143
Kraier <kraiermarin11@gmail.com>
2022-04-20 02:07:32 +0100

    Finished reciept

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('app.home');
+            return route('login');
         }
     }
 }
